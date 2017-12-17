26ed35ab309784581051e17581034ff91223625b
Alina Matsak <alina.pitaichuk@gmail.com>
2017-12-17 15:01:36 +0100

    9-operations

--- JS/1-javascript/js/9-operations.js
+++ JS/1-javascript/js/9-operations.js
@@ -1,25 +1,27 @@
 'use strict';
+//
+//var liczba1 = 13, liczba2 = 5;
+//
+//var wynik;
+//
+//wynik = liczba1 % liczba2;
+//console.log(wynik) ;
+//
+//liczba1 += liczba2;
+//console.log(liczba1);
+//
+//if ("2" == 2) {
+//    console.log ("prawda");
+//} else {
+//    console.log ("Nie prawda");
+//}
+//
+//if ( !( ("2" === 2) && (8 < 4) )) {
+//    document.write ("<br> prawda");
+//}
 
-var liczba1 = 13, liczba2 = 5;
 
-var wynik;
-
-wynik = liczba1 % liczba2;
-console.log(wynik) ;
-
-liczba1 += liczba2;
-console.log(liczba1);
-
-if ("2" == 2) {
-    console.log ("prawda");
-} else {
-    console.log ("Nie prawda");
-}
-
-if ( !( ("2" === 2) && (8 < 4) )) {
-    document.write ("<br> prawda");
-}
-
+! (8 >4 ) ? console.log('Prawda'): console.log("falsz");
 
 
 
