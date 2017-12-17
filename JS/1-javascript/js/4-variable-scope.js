83e4ef428ceeb3c729a44aff557db61ca9dabc9a
Alina Matsak <alina.pitaichuk@gmail.com>
2017-12-17 11:33:57 +0100

    variable-skope

---
+++ JS/1-javascript/js/4-variable-scope.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var ilosc1 = 3500;
+var ilosc2 = 5000;
+var ilosc3 = 7500;
+var ilosc4 = 2500;
+
+ilosc = 3000;
+
+function policzSumeKasy(iloscOsoby1, iloscOsoby2, iloscOsoby3, iloscOsoby4, wiek) {
+    
+    sumaKasy = iloscOsoby1 + iloscOsoby2 + iloscOsoby3 +iloscOsoby4;
+    
+    var osoba = [sumaKasy, wiek]
+    
+    return osoba;
+}
+
+var wynikObliczen = policzSumeKasy (ilosc3, ilosc2, ilosc1, ilosc4, wiek)
+
+console.log(  );
+
+
