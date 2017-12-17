9c3ab0a0812987550031988e135558f7b7f3888e
Alina Matsak <alina.pitaichuk@gmail.com>
2017-12-17 12:14:33 +0100

    typy danych

---
+++ JS/1-javascript/js/5-datatypes.js
@@ -0,0 +1,68 @@
+'use strict'
+
+// typ liczbowy//
+
+var wyplata = 5000;
+var premia = 2500;
+var calkowitaWyplata;
+
+calkowitaWyplata = wyplata + premia;
+
+console.log(calkowitaWyplata);
+
+//typ labcuchowy string////
+
+
+var wyplataStr = "5000";
+var premiaStr = "2500" ;
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);
+
+
+//typ logiczny//
+
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+    console.log ("Jest Smog");
+} else {
+    console.log("niema Smogu ;)")
+}
+
+
+//typy specjalne /null/underfined////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
