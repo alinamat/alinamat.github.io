25b7ad0c43c6898c30653105bcc9c913748e3f2f
Alina Matsak <alina.pitaichuk@gmail.com>
2017-12-17 11:43:12 +0100

    poprawki

--- JS/1-javascript/js/4-variable-scope.js
+++ JS/1-javascript/js/4-variable-scope.js
@@ -5,19 +5,19 @@ var ilosc2 = 5000;
 var ilosc3 = 7500;
 var ilosc4 = 2500;
 
-ilosc = 3000;
+var ilosc = 3000;
 
 function policzSumeKasy(iloscOsoby1, iloscOsoby2, iloscOsoby3, iloscOsoby4, wiek) {
     
-    sumaKasy = iloscOsoby1 + iloscOsoby2 + iloscOsoby3 +iloscOsoby4;
+var sumaKasy = iloscOsoby1 + iloscOsoby2 + iloscOsoby3 +iloscOsoby4;
     
-    var osoba = [sumaKasy, wiek]
+var osoba = [sumaKasy, wiek];
     
-    return osoba;
+return osoba;
 }
 
-var wynikObliczen = policzSumeKasy (ilosc3, ilosc2, ilosc1, ilosc4, wiek)
+var osobaZObliczeniami = policzSumeKasy (ilosc3, ilosc2, ilosc1, ilosc4, 25);
 
-console.log(  );
+console.log( osobaZObliczeniami );
 
 
