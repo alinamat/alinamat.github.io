29fda70059277dd91a53245bb926124ccca12657
Alina Matsak <alina.pitaichuk@gmail.com>
2017-12-17 12:50:29 +0100

    arrays

---
+++ JS/1-javascript/js/6-arrays.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var imiona = ['Monika', 'Krystian', 'Lukasz'];
+
+imiona[3] = 'Tania';
+imiona[2] = 'Ela';
+
+imiona.push('Grzegorz');
+
+imiona.pop();
+imiona.pop();
+
+//console.log(imiona.unshift('Robert'));
+
+//console.log(imiona.shift());
+
+//console.log(imiona);
+
+//console.log(imiona.length);
+
+//imiona.push('Kaja');
+
+//var stringZTablicy = imiona.join(' - ');
+
+//console.log(stringZTablicy);
+
+//console.log (imiona.reverse( ));
+
+
+var tablicaLiczb = [3,67,1,3,56,90];
+var posortowanaTablica = tablicaLiczb.sort();
+
+console.log(posortowanaTablica);
+
+
+
