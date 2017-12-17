6b144f4e8b705b00f16d0df2d92d941986f8ef1d
Alina Matsak <alina.pitaichuk@gmail.com>
2017-12-17 16:01:46 +0100

    petla

---
+++ JS/1-javascript/js/11-loops.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/*pętla*/
+
+//for ( var i=4 ; i>0 ; i--) {
+//    console.log(i);
+//}
+//
+//
+///*pętla forEach*/
+//
+//var tablica = [ "Krystian", "monika", "Grzegorz"];
+
+//tablica.forEach( function( element, index ) {
+//        console.log( "Element z Indexem: " + index + " ma wartosc " + element );
+//});
+//
+//for (var i=0; i<tablica.length ; i++) {
+//        console.log( "Element z Indexem: " + i + " ma wartosc " + tablica[i] );
+//};
+
+
+//Pętla while
+
+//var it = 0;
+//while ( it < tablica.length) {
+//    console.log(tablica [it] );
+//    it++;
+//}
+//
+//
+
+//Pętla do while
+
+//var iter = 2;
+//do {
+////    console.log(iter);
+//    iter++;
+//    console.log(iter);
+//} while ( item < 5 )
+//
+
+//Przerywanie pętli - break
+    
+//var a = 0;
+//while ( a < 10) {
+//    console.log(++a);
+//    
+//    if ( a == 10 ) {
+//        break;
+//    }
+//}
+
+//przeskakiwanie do kolejnej liteacji
+
+for ( var b = 0; b < 8 ; ++b ) {
+    
+    if ( b == 2 ) {
+        continue;
+    } else {
+        console.log (b);
+    }
+}
+
+
+
+
+
+
+
+
