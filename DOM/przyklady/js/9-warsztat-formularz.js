837578dd8c44bcada2035c472e0b7abbe08ed238
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-13 16:46:48 +0100

    warsztaty

---
+++ DOM/przyklady/js/9-warsztat-formularz.js
@@ -0,0 +1,15 @@
+//document.getElementsByTagName("fname", "lname").addEventListener('click', function (e) {
+//    e.preventDefault();
+//    console.log("fname", "lname");
+//});
+//
+//
+
+document.getElementById("imie", "nazwisko").addEventListener("onsubmit", function(e));
+console.log(e);
+                                                            
+function pobierzImie(e) {
+    e.preventDefault();
+    console.log(value);
+    console.log(value);
+}
