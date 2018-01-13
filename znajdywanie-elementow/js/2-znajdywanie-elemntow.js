42377cac5c3ca2072c83d09bac9854e2ff98ee0f
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-13 10:24:16 +0100

    2-znajdywanie-elementow

---
+++ znajdywanie-elementow/js/2-znajdywanie-elemntow.js
@@ -0,0 +1,18 @@
+//wyswietlanie elementu o klasie 'parFirst'
+// pobierze element o id="parFirst"
+//var parFirst = document.getElementById("parFirst");
+//console.log(parFirst);
+
+// pobierze wszystie element o class="link"
+//var linki = document.getElementByClassName("superlink");
+//console.log(superlink);
+//
+//var linkiPoTagu = document.getElementsByTagName("a");
+//console.log(linkiPoTagu);
+
+//var divPoId = document.querySelector("#parSecond");
+//console.log(divPoId);
+
+// pobierze wszystkie elementy o class="link"
+var superlinki = document.querySelectorAll( ".superlink" );
+console.log(superlinki);
