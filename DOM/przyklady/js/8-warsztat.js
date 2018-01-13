c092eda413798808e341f2bd0647c105fdff204f
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-13 16:01:20 +0100

    warsztat zadanie 1

---
+++ DOM/przyklady/js/8-warsztat.js
@@ -0,0 +1,6 @@
+var button = document.getElementById ("paragraf1", "paragraf2");
+
+function ustawTlo() {
+    document.getElementById("paragraf1").style.backgroundColor = "red";
+    document.getElementById("paragraf2").style.backgroundColor = "yellow";
+};
