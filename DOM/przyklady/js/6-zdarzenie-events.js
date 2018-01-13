8d6e4acd025cefbb6ef513f5345ca75fb945adf0
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-13 14:27:44 +0100

    zdarzenia

---
+++ DOM/przyklady/js/6-zdarzenie-events.js
@@ -0,0 +1,37 @@
+var secondLink = document.getElementsByTagName("a")[1];
+
+function alarm(e) {
+    e.preventDefault();
+    console.log("Kliknięto link, hyc - addEventListener");
+    console.log("Type eventu:" + e.type);
+}
+
+//secondLink.onclick = alarm;
+
+//secondLink.addEventListener('click', alarm);
+//secondLink.removeEventListener('click', alarm);
+
+var mainHeader = document.getElementById ("main-header");
+
+function zmianaRozmiaru(e) {
+    if( e.type == "mouseover" ) {
+        mainHeader.style.fontSize = "70px";
+    } else {
+        mainHeader.style.fontSize = "inherit";
+    }
+}
+
+mainHeader.addEventListener("mouseover", zmianaRozmiaru);
+mainHeader.addEventListener("mouseout", zmianaRozmiaru);
+
+
+document.getElementById("exampleDiv").addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("Kliknieto DIV");
+});
+
+document.getElementById("exampleLink").addEventListener("click",function (e){
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("Kliknieto link");
+})
