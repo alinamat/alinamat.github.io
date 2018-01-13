f84463ffc2d8471b2abd0a2975d1a152af4b75a8
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-13 09:50:13 +0100

    .

---
+++ budujemy-DOM/js/1-model-dom.js
@@ -0,0 +1,12 @@
+
+//console.log(window);
+
+//wyrzucamy strone do konsoli - document
+//console.log(document);
+
+//wyswietlanie poszczegolnych elementow html w konsoli
+//console.log(document.documentElement);
+
+//wyswietlanie body html-a w konsoli
+console.log(document.body);
+
