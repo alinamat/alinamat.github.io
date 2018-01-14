434f34a38f06ceb0cbecf2e9ab72dee24d07a7cc
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-14 12:39:03 +0100

    3-manipulacja html css

---
+++ JQUERY/js/3-manipulacja-dom.js
@@ -0,0 +1,67 @@
+////sprawdzamy czy wczytuje sie dom
+////wyrzucamy paragraf o id "paragraf" do konsoli
+//
+$(function(){
+//   var textParagrafu = $("p#paragraf").text("Nowy paragaf o id paragaf. Szara fretka");
+//    
+//    console.log(textParagrafu);
+////    
+//    var JakisTamParagraf = $("p.paragrafnext").html("<b>Hello</b>, World");
+//    
+////    to samo ale na inny sposob********************
+////    var JakisTamParagraf = $("p.paragrafnext")
+////    JakisTamParagraf.html("<b>Hello</b>, World");
+////*************************************************
+//    
+//    var JakisTamParagraf = $("p.paragrafnext");
+//    JakisTamParagraf.html("<b>Hello</b>, World").after("Tresc za elementem");
+//    
+//    
+////    uzycie funkcji append
+//    $ (".paragrafnext").append("<br>Czerwony wilk</br>");
+//    
+//    $("h3").remove();
+//    
+//    $("h2").empty();
+//    $("h2").html("<span style='color: red;'>Kolorowy span h2</span>");
+//    
+////    zmieniamy wielkosc czcionki w css na 100px
+//    $("h1").css("font-size", "100px");
+//    
+//    $("h2").css({"font-size": "70px", "color":"red", "background-color": "lime"});
+//    
+////    pobieramy zawartosc z pola do konsoli
+//    var imie = $("input#imie").val();
+//    console.log(imie);
+//    
+////    modyfikujemy pole formularza
+//    $("input#imie").val("Damian");
+//    
+//    
+//    $("h1").attr("title");  //pobiera atrybut
+//    $("h1").attr("title", "Naglowek h1");
+
+    
+    $("h1").addClass("blue"); //dodajemy klase
+    
+    $(".paragrafnext").removeClass("paragrafnext");
+});
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
