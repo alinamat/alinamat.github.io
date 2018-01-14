9d96acb8d1a2bc9cf2e6196e25400150336a411e
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-14 11:17:00 +0100

    selektory css

---
+++ JQUERY/js/2-selektory.js
@@ -0,0 +1,20 @@
+//pobieramy wszystkie paragrafy z html
+
+$(function(){
+    var paragrafy = $("p");
+//    console.log(paragrafy);
+    
+//    **
+    //wybieramy h2 po iD
+    //"#second" - bierzemy id o ID 'second'
+    var h2 = $("#second");
+    console.log(h2);
+    
+    var paragraf = $(".paragraf");
+    console.log = paragraf;
+});
+
+
+
+
+
