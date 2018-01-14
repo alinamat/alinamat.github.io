ad0f12c3537fff329dcc4fd25c59e1741a59d93b
Alina Matsak <alina.pitaichuk@gmail.com>
2018-01-14 10:31:12 +0100

    folder

---
+++ JS/10-warsztat-formularz-kalkulator/3-walidacja-formularza/js/main.js
@@ -0,0 +1,11 @@
+var checkboxEnd = document.getElementById( "wszystkie-zgody" );
+var checkbox = document.querySelectorAll('input[type=checkbox]');
+
+function checkboxState() {
+    document.getElementById ("wszystkie-zgody").checked = true;
+}
+
+function checkboxState() {
+    document.getElementById("wszystkie-zgody").checked = false;
+}
+
